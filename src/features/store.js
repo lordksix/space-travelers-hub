3a8ec1d85463f4d52ac5a37b676aad3997974421
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-19 14:24:56 -0500

    Add missionsSlice to store

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionReducer from 'features/missions/missionsSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    missions: missionReducer,
+  },
 });
 
 export default store;
