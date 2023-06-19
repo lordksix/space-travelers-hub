26e3261d0ab16fedb098bb9affb107bf6cba60d7
Momo-87 <momochristian87@gmail.com>
2023-06-19 10:47:05 +0100

    Add some route content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Layout from 'routes/Layout';
+import Rockets from 'routes/Rockets';
+import MissionsApp from 'routes/MissionsApp';
+import MyProfileApp from 'routes/MyProfileApp';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Rockets />}/>
-          <Route path='missions' element={<MissionsApp />}/>
-          <Route path='myprofile' element={<MyProfileApp />}/>
-          <Route path='*' element={<NotMatch />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Rockets />} />
+          <Route path="missions" element={<MissionsApp />} />
+          <Route path="myprofile" element={<MyProfileApp />} />
+          <Route path="*" element={<NotMatch />} />
         </Route>
       </Routes>
     </div>
---
+++ src/routes/Layout.js
@@ -0,0 +1,7 @@
+const Layout = () => (
+    <div>
+        <h1>Navbar Content</h1>
+    </div>
+);
+
+export default Layout;
