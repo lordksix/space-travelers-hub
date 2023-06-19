7ac3a29242555352f02836b57600a6c80ec3ce6c
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-19 05:20:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Layout from 'routes/Layout';
+import Layout from 'components/Layout';
 import Rockets from 'routes/Rockets';
 import MissionsApp from 'routes/MissionsApp';
-import MyProfileApp from 'routes/MyProfileApp';
+import ProfileApp from 'routes/ProfileApp';
 import NotMatch from 'routes/NotMatch';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Rockets />} />
           <Route path="missions" element={<MissionsApp />} />
-          <Route path="myprofile" element={<MyProfileApp />} />
+          <Route path="profile" element={<ProfileApp />} />
           <Route path="*" element={<NotMatch />} />
         </Route>
       </Routes>
