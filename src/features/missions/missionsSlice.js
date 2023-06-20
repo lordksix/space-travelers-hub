bde0414b754ca8a7a5e175ff87fb7b689ef2e595
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-20 03:06:41 -0500

    Update missionsSlice.js

--- src/features/missions/missionsSlice.js
+++ src/features/missions/missionsSlice.js
@@ -22,11 +22,8 @@ const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {
-    addSpecialist: {
-      reducer(state, action) {
-        if (state.missions.specialist.length < 4) state.missions.specialist.push(action.payload);
-        else state.missions.isAvailable = false;
-      },
+    addSpecialist: (state) => {
+      const missions =
     },
   },
   extraReducers: (builder) => {
@@ -46,11 +43,9 @@ const missionsSlice = createSlice({
           const missionId = load.mission_id;
           const missionName = load.mission_name;
           const { description } = load;
-          const specialist = [];
-          const isAvailable = specialist.length <= 3;
+          const isJoined = false;
           return {
-            specialist,
-            isAvailable,
+            isJoined,
             missionId,
             missionName,
             description,
