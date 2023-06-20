054fc9c9a1415f6b28658b27a4c10f71ab57b665
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-20 03:41:22 -0500

    Add react bootstrap styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import store from 'features/store';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
