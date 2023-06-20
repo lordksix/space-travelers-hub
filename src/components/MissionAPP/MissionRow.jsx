c6e98898e6e9e83a83eaa312f39fa05e9c424e8c
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-20 03:41:54 -0500

    Update MissionRow.jsx

--- src/components/MissionAPP/MissionRow.jsx
+++ src/components/MissionAPP/MissionRow.jsx
@@ -1,7 +1,32 @@
+import { addSpecialist } from 'features/missions/missionsSlice';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 
 const MissionRow = (props) => {
-const { missionName, missionId, description, isJoined } = props;
+  const {
+    missionName, missionId, description, isJoined,
+  } = props;
+  const dispatch = useDispatch();
+
+  const handleMissionChange = (id) => {
+    dispatch(addSpecialist(id));
+  };
+
+  let memberBtn;
+  if (isJoined) memberBtn = <Button variant="primary">Active Memeber</Button>;
+  else memberBtn = <Button variant="secondary">NOT A MEMBER</Button>;
+  let joinBtn;
+  if (isJoined) joinBtn = <Button variant="outline-danger" onClick={() => handleMissionChange(missionId)}>Leave Mission</Button>;
+  else joinBtn = <Button variant="outline-secondary" onClick={() => handleMissionChange(missionId)}>Join Mission</Button>;
+  return (
+    <tr>
+      <td>{missionName}</td>
+      <td>{description}</td>
+      <td>{memberBtn}</td>
+      <td>{joinBtn}</td>
+    </tr>
+  );
 };
 
 MissionRow.propTypes = {
