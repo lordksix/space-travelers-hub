6878742521b406daa090764dcfb521c6676777bd
Momo-87 <momochristian87@gmail.com>
2023-06-20 09:43:12 +0100

    Add extra reducer to rocketsSlice.js file

--- src/features/rockets/rocketsSlice.js
+++ src/features/rockets/rocketsSlice.js
@@ -2,10 +2,10 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const url = 'https://api.spacexdata.com/v4/rockets';
-export const fetchRockets = createAsyncThunk('rockets/fetchBooks', async (_, { rejectWithValue }) => {
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async (_, { rejectWithValue }) => {
   try {
     const response = await axios.get(url);
-    return response;
+    return response.data;
   } catch (error) {
     return rejectWithValue('Failed to fetch rockets data !!');
   }
@@ -21,5 +21,30 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        if (state.rocketsItems.length === 0) {
+          action.payload.forEach((elt) => {
+            const rocket = {
+              id: elt.id,
+              name: elt.name,
+              description: elt.description,
+              type: elt.type,
+              image: elt.flickr_images[0],
+            };
+            state.rocketsItems.push(rocket);
+          });
+        }
+        state.isLoading = false;
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      });
+  },
 });
 export default rocketsSlice.reducer;
