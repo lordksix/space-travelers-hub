0b856c0927b6112ff5b301c225a4a759344c4780
Momo-87 <momochristian87@gmail.com>
2023-06-21 09:58:07 +0100

    Display reserved rockets in My Profil

--- src/components/ProfileAPP/MyRockets.jsx
+++ src/components/ProfileAPP/MyRockets.jsx
@@ -1,6 +1,20 @@
-const MyRockets = () => (
-  <section>
-    Here comes the rockets
-  </section>
-);
-export default MyRockets;
+import { useSelector } from 'react-redux';
+import { getRockets } from 'features/rockets/rocketsSlice';
+
+const MyProfile = () => {
+  const { rocketItems } = useSelector(getRockets);
+  const bookedRockets = rocketItems.filter((rocket) => (rocket.reserved === true));
+  return (
+    <div>
+      <h1>My Rockets</h1>
+      <ul>
+        {bookedRockets.length === 0 ? <li>Rockets Not Found</li>
+          : bookedRockets.map((rocket) => (
+            <li key={rocket.id}>{rocket.name}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MyProfile;
