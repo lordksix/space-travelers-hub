476ba930dd16257d2c2b3efd72f8d6076082fa0b
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-21 13:49:46 -0500

    Add test for all SPAs

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from 'App';
 import store from 'features/store';
+import { serverMissions, serverRockets } from 'mocks/server';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -14,14 +15,65 @@ const AllTheProviders = ({ children }) => (
   </Provider>
 );
 
-test('full app rendering/navigating', async () => {
-  render(<App />, { wrapper: AllTheProviders });
-  const user = userEvent.setup();
+describe('test click mission navbar', () => {
+  beforeAll(() => serverMissions.listen());
+  afterAll(() => serverMissions.close());
+  afterEach(() => serverMissions.resetHandlers());
 
-  // verify page content for default route
-  expect(screen.getByText(/Space Traveler/i)).toBeInTheDocument();
+  test('full app rendering/navigating', async () => {
+    render(<App />, { wrapper: AllTheProviders });
 
-  // verify page content for expected route after navigating
-  await user.click(screen.getByText(/missions/i));
-  expect(screen.getByText(/(my missions)|loading|(Error:)/i)).toBeInTheDocument();
+    expect(screen.getByText(/Space Traveler/i)).toBeInTheDocument();
+  });
+
+  test('full app rendering/navigating', async () => {
+    render(<App />, { wrapper: AllTheProviders });
+    const user = userEvent.setup();
+
+    // verify page content for expected route after navigating
+    await user.click(screen.getByText(/missions/i));
+    expect(screen.getByText(/status|loading|(Error:)/i)).toBeInTheDocument();
+  });
+});
+
+describe('test click rockets navbar', () => {
+  beforeAll(() => serverRockets.listen());
+  afterAll(() => serverRockets.close());
+  afterEach(() => serverRockets.resetHandlers());
+
+  test('full app rendering/navigating', async () => {
+    render(<App />, { wrapper: AllTheProviders });
+
+    expect(screen.getByText(/Space Traveler/i)).toBeInTheDocument();
+  });
+
+  test('full app rendering/navigating', async () => {
+    render(<App />, { wrapper: AllTheProviders });
+    const user = userEvent.setup();
+
+    // verify page content for expected route after navigating
+    await user.click(screen.getByText(/rockets/i));
+    expect(screen.getAllByText(/reserv|loading|(Error:)/i).length).toBeGreaterThan(0);
+  });
+});
+
+describe('test click profile navbar', () => {
+  beforeAll(() => serverRockets.listen());
+  afterAll(() => serverRockets.close());
+  afterEach(() => serverRockets.resetHandlers());
+
+  test('full app rendering/navigating', async () => {
+    render(<App />, { wrapper: AllTheProviders });
+
+    expect(screen.getByText(/Space Traveler/i)).toBeInTheDocument();
+  });
+
+  test('full app rendering/navigating', async () => {
+    render(<App />, { wrapper: AllTheProviders });
+    const user = userEvent.setup();
+
+    // verify page content for expected route after navigating
+    await user.click(screen.getByText(/profile/i));
+    expect(screen.getByText(/my missions/i)).toBeInTheDocument();
+  });
 });
