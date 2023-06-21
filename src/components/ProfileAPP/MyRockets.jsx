bad55895d24d31bc6d6460bce31aa1545cc50b9c
Momo-87 <momochristian87@gmail.com>
2023-06-21 10:26:39 +0100

    Add styles

--- src/components/ProfileAPP/MyRockets.jsx
+++ src/components/ProfileAPP/MyRockets.jsx
@@ -1,16 +1,17 @@
 import { useSelector } from 'react-redux';
 import { getRockets } from 'features/rockets/rocketsSlice';
+import styles from 'styles/MyRockets.module.css';
 
 const MyProfile = () => {
   const { rocketItems } = useSelector(getRockets);
-  const bookedRockets = rocketItems.filter((rocket) => (rocket.reserved === true));
+  const bookedRockets = [...rocketItems.filter((rocket) => (rocket.reserved === true))];
   return (
     <div>
-      <h1>My Rockets</h1>
-      <ul>
-        {bookedRockets.length === 0 ? <li>Rockets Not Found</li>
+      <h1 className={styles.title}>My Rockets</h1>
+      <ul className={styles.rocketsList}>
+        {bookedRockets.length === 0 ? <p className={styles.noRocketFound}>Rockets Not Found</p>
           : bookedRockets.map((rocket) => (
-            <li key={rocket.id}>{rocket.name}</li>
+            <li key={rocket.id} className={styles.rocketItem}>{rocket.name}</li>
           ))}
       </ul>
     </div>
