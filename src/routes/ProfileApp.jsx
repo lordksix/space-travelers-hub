ae024bab39ba0edb25c22459cb095157272fc073
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-22 17:49:20 -0500

    Refactor profile page layout

--- src/routes/ProfileApp.jsx
+++ src/routes/ProfileApp.jsx
@@ -1,3 +1,4 @@
+import MyDragons from 'components/ProfileAPP/MyDragons';
 import MyMissions from 'components/ProfileAPP/MyMissions';
 import MyRockets from 'components/ProfileAPP/MyRockets';
 
@@ -5,15 +6,14 @@ const ProfileApp = () => {
   const myProfileStyle = {
     borderTop: 'solid 1px rgba(0, 0, 0, 0.1)',
     width: '90%',
-    display: 'grid',
-    gridTemplateColumns: '50% 50%',
-    margin: '0 5% 0 5%',
-    paddingTop: '10px',
+    margin: '0 auto',
+    paddingTop: '30px',
   };
   return (
-    <main style={myProfileStyle}>
+    <main style={myProfileStyle} className="profilePage">
       <MyMissions />
       <MyRockets />
+      <MyDragons />
     </main>
   );
 };
